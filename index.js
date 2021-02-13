400ae371541d4409381f0b7c979b420792fda860
Daniel Martorell <dani.martorell@gmail.com>
2021-02-12 21:18:30 -0600

    select player + select icon SET with animation

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 
 const gameScreen = document.querySelector('.container-game');
 const selectPlayersScreen = document.querySelector('.container-select-players');
+const selectPlayersIcon = document.querySelector('#select-player-icon');
 
-// Arranque en delay: 1 seg
+// players en delay: 1 seg
 setTimeout(() => selectPlayersScreen.style.display="grid", 1000);
+// select icon en delay 1.2 seg
+setTimeout(() => selectPlayersIcon.style.display="block", 1500);
+
+
+
 
 
