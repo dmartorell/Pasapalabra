72c9aa1c82048473f52a154ee378ec0b5d39a0ba
Daniel Martorell <dani.martorell@gmail.com>
2021-02-13 07:06:05 -0600

    eventlistener to hover on players to select and shadow

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-const gameScreen = document.querySelector('.container-game');
+// const gameScreen = document.querySelector('.container-game');
 const selectPlayersScreen = document.querySelector('.container-select-players');
 const selectPlayersIcon = document.querySelector('#select-player-icon');
 
@@ -7,6 +7,18 @@ const selectPlayersIcon = document.querySelector('#select-player-icon');
 setTimeout(() => selectPlayersScreen.style.display="grid", 1000);
 // select icon en delay 1.2 seg
 setTimeout(() => selectPlayersIcon.style.display="block", 1500);
+
+document.addEventListener('mouseover', (e) => {
+    const element = e.target;
+    if(element.className === 'p-icon') element.previousElementSibling.style.backgroundColor = '#cbdffc';    
+});
+document.addEventListener('mouseout', (e) => {
+    const element = e.target;
+    if(element.className === 'p-icon') element.previousElementSibling.style.backgroundColor = '#ecf4ff';    
+});
+
+
+
 
 
 
